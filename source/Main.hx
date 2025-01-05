b54db64d7700a03a7246b411278db258367da71d
Hadrian767-2 <hadriannfs976@gmail.com>
2025-01-04 21:38:57 -0300

    Initial commit

--- source/Main.hx
+++ source/Main.hx
@@ -82,8 +82,6 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
-
-		SUtil.check();
 	
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
